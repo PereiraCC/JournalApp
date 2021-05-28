ea914d1078515be8186a71549f3261afc9635907
PerairaCC <carloscoto0103@gmail.com>
2021-05-28 17:34:16 -0600

    Proteccion de rutas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
     Redirect,
 } from 'react-router-dom';
 
@@ -12,6 +11,8 @@ import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
 import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
 
@@ -49,13 +50,15 @@ export const AppRouter = () => {
 
             <div>
                 <Switch>
-                    <Route
+                    <PublicRoute
                         path="/auth"
                         component={AuthRouter}
+                        isAuthenticated = {isLoggedIn}
                     />
 
-                    <Route
+                    <PrivateRoute
                         exact
+                        isAuthenticated = {isLoggedIn}
                         path="/"
                         component={ JournalScreen }
                     />
